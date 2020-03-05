a27ceed8cd43ad30ec9e936a4fe753360af2b9a5
DragonMess <kmylop@hotmail.com>
2020-03-05 20:35:57 +0000

    function getAllReservations to db

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -65,10 +65,10 @@ const dbPool = new Pool({
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
-exports.getAllReservations = getAllReservations;
+// const getAllReservations = function(guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// }
+// exports.getAllReservations = getAllReservations;
 
 /// Properties
 
@@ -139,8 +139,20 @@ const addUser =  function(user) {
 }
 exports.addUser = addUser;
 
-// const userId = Object.keys(users).length + 1;
-// user.id = userId;
-// users[userId] = user;
-// return Promise.resolve(user);
-// }
+const getAllReservations = function(guest_id, limit = 10) {
+  return dbPool.query(`
+  SELECT reservations.*,properties.*
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2
+  `, [guest_id,limit])
+  .then(res => res.rows);
+}
+exports.getAllReservations = getAllReservations;
+
+
